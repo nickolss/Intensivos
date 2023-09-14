7387348d3fd901a1b932a3b736f03c736fc34023
nickolss <nickolasmaiaraujo@gmail.com>
2023-09-14 07:32:09 -0300

    Finalizado projeto com IA

--- Intensivo-IA/server/download.js
+++ Intensivo-IA/server/download.js
@@ -1,26 +1,29 @@
-import ytdl from "ytdl-core";
-import fs from "fs";
+import ytdl from "ytdl-core"
+import fs from "fs"
 
-export const download = (videoId) => {
-	const urlVideo = "https://youtube.com/shorts/" + videoId;
-	ytdl(videoId, {
-		quality: "lowestaudio", //Faz a qualidade do áudio ser baixa para o vídeo ser mais leve
-		filter: "audioonly", //Pega só o audio do vídeo
-	})
-		.on("info", (info) => {
-			// O on("info") retorna informações do vídeo pesquisado
+export const download = (videoId) =>
+  new Promise((resolve, reject) => {
+    const videoURL = "https://www.youtube.com/shorts/" + videoId
+    console.log("Realizando o download do vídeo:", videoId)
 
-			const seconds = info.formats[0].approxDurationMs;
-			if (seconds > 60) {
-				throw new Error("A duração do vídeo é maior que 60 segundos");
-			}
-		})
-		.on("end", () => {
-			// O on("end") retorna informações quando acabar o processo, no caso o download
-			// console.log("TERMINOU O DOWNLOAD")
-		})
-		.on("error", (erro) => {
-            // O on("error") retorna informações quando ocorre algum erro
-            console.log(`Ocorreu um erro: ${erro}`)
-        }).pipe(fs.createWriteStream("./tmp/audio.mp4"));
-};
+    ytdl(videoURL, { quality: "lowestaudio", filter: "audioonly" })
+      .on("info", (info) => {
+        const seconds = info.formats[0].approxDurationMs / 1000
+
+        if (seconds > 60) {
+          throw new Error("A duração desse vídeo é maior do que 60 segundos.")
+        }
+      })
+      .on("end", () => {
+        console.log("Download do vídeo finalizado.")
+        resolve()
+      })
+      .on("error", (error) => {
+        console.log(
+          "Não foi possível fazer o download do vídeo. Detalhes do erro:",
+          error
+        )
+        reject(error)
+      })
+      .pipe(fs.createWriteStream("./tmp/audio.mp4"))
+  })
